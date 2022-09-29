a17d7bf009c9dea7ac3e057ed84bf92ba7085439
Dominik Beste <41393569+Tharylia@users.noreply.github.com>
2022-09-29 15:49:12 +0200

    Prepare for text area control

---
+++ Estreya.BlishHUD.Shared/Controls/TextArea.cs
@@ -0,0 +1,33 @@
+﻿namespace Estreya.BlishHUD.Shared.Controls;
+
+using Blish_HUD.Controls;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TextArea : TextInputBase
+{
+    public override int GetCursorIndexFromPosition(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void MoveLine(int delta)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Paint(SpriteBatch spriteBatch, Rectangle bounds)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void UpdateScrolling()
+    {
+        throw new NotImplementedException();
+    }
+}
