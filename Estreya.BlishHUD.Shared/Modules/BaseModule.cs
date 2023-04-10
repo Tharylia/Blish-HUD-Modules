4ab367547953a9b28c377d6c2974d247f2de4ef7
Tharylia <41393569+Tharylia@users.noreply.github.com>
2023-04-10 17:49:23 +0200

    change corner icon priority

--- Estreya.BlishHUD.Shared/Modules/BaseModule.cs
+++ Estreya.BlishHUD.Shared/Modules/BaseModule.cs
@@ -360,7 +360,7 @@ public abstract class BaseModule<TModule, TSettings> : Module where TSettings : 
                 {
                     IconName = this.Name,
                     Icon = this.GetCornerIcon(),
-                    Priority = this.Name.GetHashCode()
+                    Priority = 1_289_351_278
                 };
 
                 this.OnCornerIconBuild();
