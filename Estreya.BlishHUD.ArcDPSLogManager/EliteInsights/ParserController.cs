6e94ab17d400f36b0d08fb30404cf965c4ba517a
Tharylia <41393569+Tharylia@users.noreply.github.com>
2023-04-26 13:38:38 +0200

    Add prototype module arcdps log manager

---
+++ Estreya.BlishHUD.ArcDPSLogManager/EliteInsights/ParserController.cs
@@ -0,0 +1,23 @@
+﻿namespace Estreya.BlishHUD.ArcDPSLogManager.EliteInsights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ParserController : GW2EIEvtcParser.ParserController
+{
+    private IProgress<string> _progress;
+
+    public ParserController(IProgress<string> progress)
+    {
+        this._progress = progress;
+    }
+
+    public override void UpdateProgress(string status)
+    {
+        base.UpdateProgress(status);
+
+        this._progress?.Report(status);
+    }
+}
