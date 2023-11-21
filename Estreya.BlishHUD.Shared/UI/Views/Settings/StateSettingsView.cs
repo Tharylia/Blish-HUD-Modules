5adfab2cf2cbcf9b2d2b653e7ba7018f9a82227b
Tharylia <41393569+Tharylia@users.noreply.github.com>
2023-11-21 18:20:46 +0100

    Redesign state view

--- Estreya.BlishHUD.Shared/UI/Views/Settings/StateSettingsView.cs
+++ Estreya.BlishHUD.Shared/UI/Views/Settings/StateSettingsView.cs
@@ -1,16 +1,23 @@
 ﻿namespace Estreya.BlishHUD.Shared.UI.Views.Settings;
 
+using Blish_HUD;
 using Blish_HUD.Controls;
 using Blish_HUD.Modules.Managers;
+using Estreya.BlishHUD.Shared.Models;
 using Microsoft.Xna.Framework;
 using MonoGame.Extended.BitmapFonts;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Services;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 public class ServiceSettingsView : BaseSettingsView
 {
+    private const int LABEL_VALUE_X_LOCATION = 200;
     private readonly Func<Task> _reloadCalledAction;
     private readonly IEnumerable<ManagedService> _stateList;
 
@@ -20,38 +27,22 @@ public class ServiceSettingsView : BaseSettingsView
         this._reloadCalledAction = reloadCalledAction;
     }
 
+    protected override Dictionary<ControlType, BitmapFont> ControlFonts {
+        get
+        {
+            var fonts = base.ControlFonts;
+            fonts[ControlType.Label] = GameService.Content.DefaultFont18;
+
+            return fonts;
+        }
+    }
+
     protected override void BuildView(FlowPanel parent)
     {
+        parent.CanScroll = true;
         foreach (ManagedService state in this._stateList)
         {
-            List<Type> baseTypes = new List<Type>();
-
-            Type baseType = state.GetType().BaseType;
-
-            while (baseType != null)
-            {
-                if (baseType.IsGenericType)
-                {
-                    baseTypes.Add(baseType.GetGenericTypeDefinition());
-                }
-                else
-                {
-                    baseTypes.Add(baseType);
-                }
-
-                baseType = baseType.BaseType;
-            }
-
-            if (state.GetType().BaseType.IsGenericType && baseTypes.Contains(typeof(APIService<>)))
-            {
-                bool loading = (bool)state.GetType().GetProperty(nameof(APIService<object>.Loading)).GetValue(state);
-                bool finished = state.Running && !loading;
-                this.RenderLabel(parent, $"{state.GetType().Name} running & loaded:", finished.ToString(), textColorValue: finished ? Color.Green : Color.Red);
-            }
-            else
-            {
-                this.RenderLabel(parent, $"{state.GetType().Name} running:", state.Running.ToString(), textColorValue: state.Running ? Color.Green : Color.Red);
-            }
+            this.RenderState(state, parent);
         }
 
         if (this._reloadCalledAction != null)
@@ -61,6 +52,74 @@ public class ServiceSettingsView : BaseSettingsView
 
             this.RenderButtonAsync(parent, "Reload", this._reloadCalledAction);
         }
+    }
+
+    private void RenderState(ManagedService managedService, FlowPanel parent)
+    {
+        var isAPIState = this.IsAPIState(managedService);
+
+        var title = managedService.GetType().Name;
+        if (isAPIState) title += " - API State";
+
+        FlowPanel stateGroup = new FlowPanel()
+        {
+            Parent = parent,
+            FlowDirection = ControlFlowDirection.SingleTopToBottom,
+            HeightSizingMode = SizingMode.AutoSize,
+            OuterControlPadding = new Vector2(20, 20),
+            ShowBorder = true,
+            Title = title
+        };
+        stateGroup.Width = parent.ContentRegion.Width - (int)stateGroup.OuterControlPadding.X * 2;
+
+        var configuration = (ServiceConfiguration)managedService.GetType().GetProperties(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).Last(x => x.Name == "Configuration").GetValue(managedService);
+        this.RenderLabel(stateGroup, "Configuration:", JsonConvert.SerializeObject(configuration,Formatting.Indented, new JsonSerializerSettings
+        {
+            Converters = new JsonConverter[]
+            {
+                new StringEnumConverter()
+            }
+        }), valueXLocation: LABEL_VALUE_X_LOCATION);
+
+        this.RenderLabel(stateGroup, "Running:", managedService.Running.ToString(), textColorValue: managedService.Running ? Color.Green : Color.Red, valueXLocation: LABEL_VALUE_X_LOCATION);
+
+        if (isAPIState)
+        {
+            bool loading = (bool)managedService.GetType().GetProperty(nameof(APIService<object>.Loading)).GetValue(managedService);
+            bool finished = managedService.Running && !loading;
+            this.RenderLabel(stateGroup, $"Loading finished:", finished.ToString(), textColorValue: finished ? Color.Green : Color.Red, valueXLocation: LABEL_VALUE_X_LOCATION);
+
+            string progressText = (string)managedService.GetType().GetProperty(nameof(APIService<object>.ProgressText)).GetValue(managedService);
+            this.RenderLabel(stateGroup, $"Progress Text:", progressText, valueXLocation: LABEL_VALUE_X_LOCATION);
+        }
+        else
+        {
+        }
+
+        this.RenderEmptyLine(stateGroup, (int)stateGroup.OuterControlPadding.X);
+    }
+
+    private bool IsAPIState(ManagedService managedService)
+    {
+        List<Type> baseTypes = new List<Type>();
+
+        Type baseType = managedService.GetType().BaseType;
+
+        while (baseType != null)
+        {
+            if (baseType.IsGenericType)
+            {
+                baseTypes.Add(baseType.GetGenericTypeDefinition());
+            }
+            else
+            {
+                baseTypes.Add(baseType);
+            }
+
+            baseType = baseType.BaseType;
+        }
+
+        return managedService.GetType().BaseType.IsGenericType && baseTypes.Contains(typeof(APIService<>));
     }
 
     protected override Task<bool> InternalLoad(IProgress<string> progress)
