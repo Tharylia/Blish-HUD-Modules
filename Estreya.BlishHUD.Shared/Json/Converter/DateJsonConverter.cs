40ef882f3cfbcd512903694787cf5345d7498ae1
Tharylia <41393569+Tharylia@users.noreply.github.com>
2024-09-23 19:48:29 +0200

    Fix date parsing error

--- Estreya.BlishHUD.Shared/Json/Converter/DateJsonConverter.cs
+++ Estreya.BlishHUD.Shared/Json/Converter/DateJsonConverter.cs
@@ -1,11 +1,13 @@
 ﻿namespace Estreya.BlishHUD.Shared.Json.Converter;
 
 using Newtonsoft.Json.Converters;
+using System.Globalization;
 
 public class DateJsonConverter : IsoDateTimeConverter
 {
     public DateJsonConverter()
     {
         this.DateTimeFormat = "yyyy-MM-dd";
+        this.Culture = CultureInfo.InvariantCulture;
     }
 }
